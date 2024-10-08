ec482bb08c71661995bd1420a36014750969e3e2
qstar-inc <qstar.inc@gmail.com>
2024-10-09 01:48:54 +0600

    change to live server

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -1,5 +1,5 @@
-// var u = "aHR0cHM6Ly9zay1rbS5jb20uYmQv"; //web
-var u = "aHR0cDovL2xvY2FsaG9zdDoxNTAv"; //local
+var u = "aHR0cHM6Ly9zay1rbS5jb20uYmQv"; //web
+// var u = "aHR0cDovL2xvY2FsaG9zdDoxNTAv"; //local
 var auth = "ZUJImMyi0d7ycNC9Nnrixku9WM4nbWY9";
 
 function random(min, max) {
