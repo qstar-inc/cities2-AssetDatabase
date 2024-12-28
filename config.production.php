4a5ad0e6fb148dccf6b4def134ceea321acead28
qstar-inc <qstar.inc@gmail.com>
2024-12-28 23:29:17 +0600

    PDX Mods link, credits and window size limiter

--- config.production.php
+++ config.production.php
@@ -6,9 +6,9 @@ $repo = "/cities2-AssetDatabase";
 return [
     'production' => true,
     'baseUrl' => $baseUrl,
-    'title' => 'Work-In-Progress',
-    // 'title' => 'Cities: Skylines II Asset Database',
-    // 'description' => 'A complete files for Cities: Skylines II',
+    // 'title' => 'Work-In-Progress',
+    'title' => 'Cities: Skylines II Asset Database',
+    'description' => 'A complete asset database for Cities: Skylines II',
     'collections' => [],
     'url' => $baseUrl,
     'keywords' => 'citiesskylines2, cities-skylines-2, cs2, skylines2, cities2',
